1d3f234c0acb0ff35ef2b5fee5339a8ecf880782
独行工匠 <515235416@qq.com>
2019-09-25 23:10:13 +0800

    添加代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,14 @@
-var ljsd = "生产完成/健身助手",
+var ljsd = "生产完成/会计",
     ty = "pc"
 
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
         fixturesPath: './src/' + ty,
-        postcss: { //css加前缀
-            options: {
-                map: false,
-                processors: [
-
-                  require('autoprefixer')(),
-                  require('cssgrace')
-            ]
-            },
-            dist: {
-                src: [ljsd + '/css/style.css', ljsd + '/css/base.css']
-            }
-        },
-        apidoc: {
-            mypp: {
-                src: "app/",
-                dest: "apidoc/",
-                options: {
-                    debug: true,
-                    includeFilters: [".*\\.js$"],
-                    excludeFilters: ["node_modules/"]
-                }
-            }
-        },
-        cssmin: {
-            target: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/' + ty + '/css',
-                        src: '**/*.css',
-                        dest: ljsd + '/css'
-                    }
-
-                       ]
-            }
-        },
-
-        htmlbuild: {
+         htmlbuild: {
             //            html上这样引入 就可以插入 头部和尾部
-            //   <!-- build:section layout.header -->
-            //    <!-- /build -->
-            //    <!-- build:section layout.footer -->
-            //    <!-- /build -->
+            //   <!-- build:section layout.header --> <!-- /build -->
+            //    <!-- build:section layout.footer --><!-- /build -->
             //这样就可以加版本号了
             // <!-- build:process -->
             //    <script src="js/jquery-1.11.3.min.js?v=<%= version %>"></script>
@@ -67,14 +27,41 @@ module.exports = function (grunt) {
                 options: {
                     sections: {
                         layout: {
-                            header: '<%= fixturesPath %>/layout/header.html',
-                            footer: '<%= fixturesPath %>/layout/footer.html'
+                            header: '<%= fixturesPath %>/lay/top.html',
+                            footer: '<%= fixturesPath %>/lay/bottom.html'
                         }
                     },
                     data: {
                         version: "0.1.0",
                     },
                 }
+            }
+        },
+         postcss: { //css加前缀
+            options: {
+                map: false,
+                processors: [
+
+                  require('autoprefixer')(),
+                  require('cssgrace')
+            ]
+            },
+            dist: {
+                src: [ljsd + '/css/style.css', ljsd + '/css/base.css']
+            }
+        },
+        htmlmin: { // 压缩html
+            dist: { // Target 
+                options: { // Target options 
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                  expand: true,
+                        cwd: './src/' + ty,
+                        src: '**/*.html',
+                        dest: ljsd
+                }]
             }
         },
         copy: {
@@ -102,47 +89,57 @@ module.exports = function (grunt) {
             }
         },
 
-        uglify: { //压缩
-            buildall: {
-                files: [{
+        cssmin: {
+            target: {
+                files: [
+                    {
                         expand: true,
-                        cwd: 'src/' + ty + '/js', //js目录下
-                        src: ['*.js'], //所有js文件
-                        dest: ljsd + '/js' //输出到此目录下
-                    }, {
-                        expand: true,
-                        cwd: ljsd + '/js', //js目录下
-                        src: ['page/*.js', 'page/**/*.js', "components/*.js"], //所有js文件
-                        dest: ljsd + '/js' //输出到此目录下
+                        cwd: 'src/' + ty + '/css',
+                        src: '**/*.css',
+                        dest: ljsd + '/css'
                     }
 
-                           ]
+                       ]
             }
-
         },
         babel: {
             options: {
-                sourceMap: false,
+                sourceMap: true,
                 presets: ['babel-preset-es2015']
             },
             dist: {
                 files: [{
                     expand: true,
-                    cwd: 'src/' + ty + '/js', //js目录下
-                    src: ['page/*.js', 'page/**/*.js', "components/*.js"], //所有js文件
-                    dest: ljsd + '/js' //输出到此目录下
+                    cwd: 'src/' + ty + '/js/page', //js目录下
+                    src: '*.js', //所有js文件
+                    dest: ljsd + '/js/page' //输出到此目录下
              }]
             }
         },
-//        apidoc: {
-//            myapp: {
-//                src: "app/",
-//                dest: "apidoc/"
-//            }
-//        }
-    });
-    grunt.loadNpmTasks('grunt-babel');
+        uglify: { //压缩
+            buildall: { //按原文件结构压缩js文件夹内所有JS文件
+                files: [{
+                             expand: true,
+                             cwd: 'src/' + ty + '/js', //js目录下
+                             src: '*.js', //所有js文件
+                             dest: ljsd + '/js' //输出到此目录下
+                      }]
+//                files: [{
+//                        expand: true,
+//                        cwd: ljsd + '/js', //js目录下
+//                        src: '*.js', //所有js文件
+//                        dest: ljsd + '/js' //输出到此目录下
+//                    }
+//
+//                           ]
+            }
 
+        },
+
+
+
+
+    });
     grunt.loadNpmTasks('grunt-babel');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -151,10 +148,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-html-build');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-postcss')
-  grunt.loadNpmTasks('apidoc');
-//    grunt.loadNpmTasks('grunt-apidoc')
-    //    grunt.registerTask('default', ["babel"]);
-    //     ""
-      grunt.registerTask('default', ['cssmin','copy','uglify',"htmlbuild"]);
+
+       grunt.loadNpmTasks('grunt-postcss')
+
+
+    grunt.registerTask('default', ["htmlbuild","copy","imagemin","uglify","cssmin", 'postcss']);
 }
---
+++ src/pc/README.md
@@ -0,0 +1,61 @@
+##brow
+ <div class="row">
+        <div class="col-xs-3"></div>
+</div>
+
+
+##bbtn 按钮
+<button type="button" class="btn btn-primary btn-lg">下一步</button>
+
+##bduoxuan 多选
+  <span class="dfdg_deerrt">
+                    <i class="f_i gouxuanseer"></i>
+                    我已阅读并同意
+                </span>
+        
+        
+## bllist 左侧的列表
+<div class="list-group">
+                    <a href="#" class="list-group-item active fz20">
+                        悬赏提问
+                    </a>
+                    <a href="#" class="list-group-item">同行业解答</a>
+                    <a href="#" class="list-group-item act">专业老师解答</a>
+                    <a href="#" class="list-group-item">&nbsp;</a>
+                </div>
+                
+                
+
+## bmianban 面板
+<div class="panel panel-default">
+                <div class="panel-heading">上传资格证书照片</div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
+            
+
+## html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>  </title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/base.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+     <!--[if IE 7]>    <link href="../css/ie7.css" rel="stylesheet"><![endif]--> 
+    
+</head>
+
+<body>
+    <!-- build:section layout.header --> <!-- /build -->
+
+<!-- build:section layout.footer --><!-- /build -->
+    <script src="../js/jquery-1.11.3.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/base.js"></script>
+</body></html>
